0d5ee39713da2be479fd0cec2a1858f9351d44fd
dragoonzx <dragoonz@yandex.ru>
2019-03-25 20:03:57 +0300

    tinder button proto

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+var io = require('socket.io')(https);
+var a = [];
 
 var app = express();
 
@@ -30,8 +32,42 @@ app.use(express.static(__dirname + '/client/build'));
 
 app.get('/client', function(req, res){
     res.sendFile(__dirname + '/client/build/index.html');
-    console.log('Ii`s working');
+    console.log('It`s working');
     });
+
+//socket.io
+io.sockets.on('connection', function (socket) {
+  socket.on('eventServer', function (data) {
+  
+  console.log('user connected '+ socket.id);
+  // io.emit('eventClient', { for: 'everyone', data: 'sosite chlen' });
+  console.log(data);
+  if (a == false) {
+    console.log(1);
+  a.push({info:data.info,time: data.time});
+  setTimeout(timeCheck,3500)
+  }else if (a[0].time > data.time - 3500) {
+    console.log(2);
+  a.push({info:data.info,time:data.time});
+  }else {
+    console.log(3);
+  a.push({});
+  io.emit('eventClient', { info: a, message: 'передача закончилась' });
+  a = [];
+  }
+  // console.log('u was disco');
+  // socket.emit('eventClient', { data: 'Hello Client' + data.time });
+  });
+  socket.on('disconnect', function () {
+  console.log('user disconnected');
+  });
+});
+
+function timeCheck(){
+	io.emit('eventClient', { info: a, message: 'передача закончилась' });
+	a=[];
+}
+//end of socket.io
 
 //Function for checking https-redirecting-ignoring 
 app.get('/insecure', function (req, res) {
@@ -42,4 +78,4 @@ var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(port, () => console.log('server is listening'));
-httpsServer.listen(sport, () => console.log('server is listening savely'));
+httpsServer.listen(sport, () => console.log('server is listening safely'));
