fe1d66fb3465a535c9544f95bce3740d470ef56a
dragoonzx <dragoonz@yandex.ru>
2019-03-25 12:51:19 +0300

    fix port #3

--- index.js
+++ index.js
@@ -6,13 +6,28 @@ const normalizePort = require('normalize-port');
 
 var app = express();
 
-var port = 80;
+var port = 8000;
 var sport = 443;
 
 var privateKey  = fs.readFileSync('improveyourself.key', 'utf8');
 var certificate = fs.readFileSync('improveyourself.crt', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(port);
+httpsServer.listen(sport);
+
+app.all('*', ensureSecure);
+
+function ensureSecure(req, res, next){
+    if(req.secure){
+      return next();
+    };
+    res.redirect('https://' + req.hostname + req.url);
+}
 
 app.get('/', (req,res) => {
     const users = [
@@ -22,9 +37,4 @@ app.get('/', (req,res) => {
     res.json(users);
 })
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(port);
-httpsServer.listen(sport);
 
