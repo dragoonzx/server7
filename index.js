980081209c8d75525548b49d6aaecb26f7df6669
dragoonzx <dragoonz@yandex.ru>
2019-03-25 12:22:41 +0300

    fix port

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
+const normalizePort = require('normalize-port');
 
 var app = express();
 
-var port = 8000;
-var sport = 8443;
+var port = normalizePort(process.env.PORT || '80');
+var sport = normalizePort(process.env.PORT || '443');
 
 var privateKey  = fs.readFileSync('improveyourself.key', 'utf8');
 var certificate = fs.readFileSync('improveyourself.crt', 'utf8');
@@ -15,7 +16,7 @@ var credentials = {key: privateKey, cert: certificate};
 
 app.get('/', (req,res) => {
     const users = [
-      {id:1, name:'Max'}  
+      {id:1, name:'Erzhan', action:"Vstavai blyad"}  
     ]
 
     res.json(users);
